dce4b7210a7dcfdeb1566510c9a07597468383e9
Palash Bera <palashbera1234@gmail.com>
2020-05-13 00:21:44 +0530

    Adding file manager page

---
+++ src/pages/FileManagerPage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function FileManagerPage() {
+  return (
+    <div className='row justify-content-center'>
+      <div className='card my-5'>
+        <div className='card-body'>
+          File Manager Page
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default FileManagerPage;
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,13 @@ import { Route, Switch } from 'react-router-dom';
 import HomePage from '../pages/HomePage';
 import LoginPage from '../pages/LoginPage';
 import SignupPage from '../pages/SignupPage';
+import FileManagerPage from '../pages/FileManagerPage';
 
 export default (
   <Switch>
     <Route exact path='/' component={HomePage} />
     <Route exact path='/login' component={LoginPage} />
     <Route exact path='/signup' component={SignupPage} />
+    <Route exact path='/file_manager' component={FileManagerPage} />
   </Switch>
 );
